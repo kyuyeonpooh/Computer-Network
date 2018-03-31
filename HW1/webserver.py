d8b79aa3528b9d44bb3c8fd36241a3f6b63c8b75
Kyuyeon <kyuyeonpooh@gmail.com>
2018-04-01 03:00:14 +0900

    handled 404 Not Found

--- HW1/webserver.py
+++ HW1/webserver.py
@@ -14,10 +14,9 @@ class SimpleServer:
     # initialize socket
     def init_socket(self):
         self.__socket = socket(AF_INET, SOCK_STREAM)
-        self.__socket = socket(AF_INET, SOCK_STREAM)
         self.__socket.bind((self.__host_addr, self.__port))
         self.__socket.listen(10)
-        print("TCP Server is ready for port number {port}.".format(port=self.__port))
+        print("TCP Server is ready for port number {}.\n".format(self.__port))
 
     # accept and handle client's request
     def serve_request(self):
@@ -33,17 +32,20 @@ class SimpleServer:
                     file_name = "index.html"
                 else:
                     file_name = file_name[1:]
-                print("File requested: {path}".format(path=file_name))
+                print("File requested: {}".format(file_name))
 
                 # open and get the file content
                 file, status_code = self.load_file(file_name)
-                print("Status: {status}".format(status=status_code))
+                print("Status: {}".format(status_code))
 
                 # send header and the file content
                 self.__conn_socket.send(self.get_header(file_name, status_code).encode())
                 if file is not None:
                     self.__conn_socket.send(file.read())
+                else:
+                    self.__conn_socket.send("404 Not Found".encode())
 
+            # finish the request with closing socket
             self.__conn_socket.close()
             print("Request process ended.\n")
 
@@ -60,13 +62,13 @@ class SimpleServer:
         try:
             file_size = os.path.getsize(file_name)
         except FileNotFoundError:
-            file_size = 0
-        _, extension = os.path.splitext(file_name)
-        print("Content-Type: {}".format(mimetypes.types_map.get(extension, "application/octet-stream")))
+            file_size = len("404 Not Found")
+        _, file_ext = os.path.splitext(file_name)
+        print("Content-Type: {}".format(self.get_file_type(file_ext, status_code)))
         print("Content-Length: {} bytes".format(file_size))
         return "\r\n".join([
             "HTTP/1.1 {} {}".format(status_code, self.get_status_str(status_code)),
-            "Content-Type: {}".format(mimetypes.types_map.get(extension, "application/octet-stream")),
+            "Content-Type: {}".format(self.get_file_type(file_ext, status_code)),
             "Content-Length: {}".format(file_size)
         ]) + "\r\n\r\n"
 
@@ -76,6 +78,12 @@ class SimpleServer:
             return "OK"
         else:
             return "Not Found"
+
+    def get_file_type(self, file_ext, status_code):
+        if status_code == 200:
+            return mimetypes.types_map.get(file_ext, "application/octet-stream")
+        else:
+            return "text/plain"
 
 
 if __name__ == "__main__":
